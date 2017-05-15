a372e42a3c1eb730ba40a3b6dfdecd811e2a64e4
JCYTopWorld <jcyemail@qq.com>
2017-05-15 21:51:27 +0800

    坦克大战
    添加源码

---
+++ Tank/Tank/Assets/Sences/GameSence/Scripts/AI.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+public enum Status
+{
+    Patrol,
+    Attack
+}
+
+public class AI : MonoBehaviour
+{
+    public Tank tank;
+    private Status status = Status.Patrol;
+    private GameObject target;
+    private float sightDistance = 30;
+    private float lastSearchTargetTime = 0;
+    private float searchTargetInterval = 3;
+    private Path path = new Path();
+
+    void Start()
+    {
+        InitWaypoint();
+    }
+
+    void Update()
+    {
+        if (tank.ctrlType != Tank.CtrlType.computer) return;
+        TargetUpdate();
+
+        //行走
+        if (path.IsReach(transform))
+        {
+            path.NextWaypoint();
+        }
+
+        if (status == Status.Patrol)
+        {
+            PatrolUpdate();
+        }
+        else if (status == Status.Attack)
+        {
+            AttackUpdate();
+        }
+    }
+
+    //初始化路径
+    void InitWaypoint()
+    {
+        GameObject obj = GameObject.Find("WaypointContainer");
+        if (obj)
+        {
+            path.InitByObj(obj);
+        }
+    }
+
+    //搜索目标
+    private void TargetUpdate()
+    {
+        float interval = Time.time - lastSearchTargetTime;
+        if (interval < searchTargetInterval) return;
+        if (target != null) HasTarget();
+        else Notarget();
+    }
+
+    //已经发现坦克目标的情况下
+    private void HasTarget()
+    {
+        Tank targetTank = target.GetComponent<Tank>();
+        Vector3 pos = transform.position;
+        Vector3 targetpos = target.transform.position;
+        if (targetTank.ctrlType == Tank.CtrlType.none)
+        {
+            Debug.Log("目标死亡，丢失目标！！！");
+            target = null;
+        }
+        else if (Vector3.Distance(pos, targetpos) > sightDistance)
+        {
+            Debug.Log("距离太远，丢失目标！！！");
+            target = null;
+        }
+    }
+
+    //没有发现坦克目标情况下
+    private void Notarget()
+    {
+        float minHp = float.MaxValue;
+        GameObject[] targets = GameObject.FindGameObjectsWithTag("Tank");
+        //Debug.Log(targets.Length);
+        for (int i = 0; i < targets.Length; i++)
+        {
+            Tank tank = targets[i].GetComponent<Tank>();
+            if (tank == null)
+            {
+                continue;
+            }
+            if (targets[i] == gameObject)
+            {
+                continue;
+            }
+            if (tank.ctrlType == Tank.CtrlType.none)
+            {
+                continue;
+            }
+            //判断距离
+            Vector3 pos = transform.position;
+            Vector3 targetPos = targets[i].transform.position;
+            if (Vector3.Distance(pos, targetPos) > sightDistance)
+            {
+                continue;
+            }
+            //判断生命值
+            if (minHp > tank.hp)
+            {
+                target = tank.gameObject;
+            }
+        }
+        //调试
+        if (target != null)
+        {
+            Debug.Log("获取目标" + target.name);
+        }
+    }
+
+    //被攻击,仇恨系统
+    public void OnAttecked(GameObject attackTank)
+    {
+        target = attackTank;
+    }
+
+    public void ChangeStatus(Status status)
+    {
+        if (status == Status.Patrol)
+        {
+            PartrolStart();
+        }
+        else if (status == Status.Attack)
+        {
+            AttackStart();
+        }
+    }
+
+    //巡逻逻辑
+    private void PartrolStart()
+    {
+
+    }
+
+    //攻击逻辑
+    private void AttackStart()
+    {
+
+    }
+
+    //巡逻中
+    private void PatrolUpdate()
+    {
+
+    }
+
+    //攻击中
+    private void AttackUpdate()
+    {
+
+    }
+
+    //获取炮管和炮塔的目标角度
+    public Vector3 GetTurretTarget()
+    {
+        //没有目标，炮塔朝向坦克的前方
+        if (target == null)
+        {
+            float y = transform.eulerAngles.y;
+            Vector3 rot = new Vector3(0, y, 0);
+            return rot;
+        }
+        //有目标，炮塔对准目标
+        else
+        {
+            Vector3 pos = transform.position;
+            Vector3 targetpos = target.transform.position;
+            Vector3 vec = targetpos - pos;
+            return Quaternion.LookRotation(vec).eulerAngles;
+        }
+    }
+
+    //是否发射炮弹
+    public bool IsShot()
+    {
+        if (target == null) return false;
+
+        //目标角度差
+        float turretRoll = tank.turret.eulerAngles.y;
+        float angle = turretRoll - GetTurretTarget().y;
+        if (angle < 0) angle += 360;
+        //30角度内可以发射炮弹
+        if (angle < 5 || angle > 355)
+            return true;
+        else
+            return false;
+    }
+
+    //获取转向角
+    public float GetSteering()
+    {
+        if (tank == null) return 0;
+        Vector3 itp = transform.InverseTransformPoint(path.waypoint);
+        if (itp.x > path.deviation / 5) return tank.maxSteeringAngle;
+        else if (itp.x < -path.deviation / 5) return -tank.maxSteeringAngle;
+        else return 0;
+    }
+
+    //获取马力
+    public float GetMotor()
+    {
+        if (tank == null)
+            return 0;
+
+        Vector3 itp = transform.InverseTransformPoint(path.waypoint);
+        float x = itp.x;
+        float z = itp.y;
+        float r = 6;
+
+        if (z < 0 && Mathf.Abs(x) < -z && Mathf.Abs(x) < r) return -tank.maxMotorTorque;
+        else return tank.maxMotorTorque;
+    }
+
+    //获取刹车
+    public float GetBrakeTorque()
+    {
+        if (path.isFinish) return tank.maxMotorTorque;
+        else return 0;
+    }
+}
---
+++ Tank/Tank/Assets/Sences/GameSence/Scripts/CameraFollow.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraFollow : MonoBehaviour
+{
+    //纵向旋转速度
+    private float rollSpeed = 0.08f;
+    //横向旋转速度
+    public float rotSpeed = 0.18f;
+    //纵向角度
+    private float roll = 30f * Mathf.PI * 2 / 360;
+    //横向角度
+    public float rot = 0;
+    //距离
+    public float distance = 8;
+    //目标物体
+    private GameObject target;
+    //纵向角度范围
+    private float maxRoll = 70f * Mathf.PI * 2 / 360;
+    private float minRoll = -10f * Mathf.PI * 2 / 360;
+    //距离范围
+    public float maxDistance = 10f;
+    public float minDistance = 5f;
+    //距离变化速度
+    public float zoomSpeed = 0.2f;
+    void Start()
+    {
+        //找到坦克
+        //target = GameObject.Find("Tank");
+        SetTarget(GameObject.Find("Tank"));
+    }
+
+    private void SetTarget(GameObject target)
+    {
+        if (target.transform.FindChild("cameraPoint") != null)
+            this.target = target.transform.FindChild("cameraPoint").gameObject;
+        else
+            this.target = target;
+    }
+
+    void LateUpdate()
+    {
+        //一些判断
+        if (target == null)
+            return;
+        if (Camera.main == null)
+            return;
+        //目标的坐标
+        Vector3 targetPos = target.transform.position;
+        //用三角函数计算相机位置
+        Vector3 cameraPos;
+        float d = distance * Mathf.Cos(roll);
+        float height = distance * Mathf.Sin(roll);
+        cameraPos.x = targetPos.x + d * Mathf.Cos(rot);//计算出X的坐标位置
+        cameraPos.y = targetPos.y + height;//计算出Y的坐标位置
+        cameraPos.z = targetPos.z + d * Mathf.Sin(rot);
+        Camera.main.transform.position = cameraPos;
+        //对准目标
+        Camera.main.transform.LookAt(target.transform);
+        //纵向旋转
+        Rotate();
+        //横向旋转
+        Roll();
+        //调整距离
+        Zoom();
+    }
+
+    private void Zoom()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (distance > minDistance)
+                distance -= zoomSpeed;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (distance < maxDistance)
+                distance += zoomSpeed;
+        }
+    }
+
+    private void Roll()
+    {
+        float w = Input.GetAxis("Mouse Y") * rollSpeed * 0.5f;
+
+        roll -= w;
+        if (roll > maxRoll)
+            roll = maxRoll;
+        if (roll < minRoll)
+            roll = minRoll;
+    }
+
+    private void Rotate()
+    {
+        float w = Input.GetAxis("Mouse X") * rotSpeed;
+        rot -= w;
+    }
+}
+ 
